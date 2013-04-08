c6c534c0ced65cfeff619fb4a1642461ab16d1a5
srirampc <sriram.pc@gmail.com>
2013-04-08 20:09:03 +0530

    Update theme to not to set fonts; Org-mode for xelatex

--- power/config/theme-conf.el
+++ power/config/theme-conf.el
@@ -1,8 +1,7 @@
 ;;
-(if (equalp (symbol-name system-type) "gnu/linux")
-    (set-default-font
-     "-outline-Inconsolata-normal-r-normal-normal-16-97-96-96-c-*-iso10646-1")
-  (set-default-font "Monaco-12"))
+ (if (equalp (symbol-name system-type) "gnu/linux")
+     (set-default-font "Inconsolata-12")
+   (set-default-font "Monaco-12"))
 
 ;;
 (setq custom-theme-directory (live-pack-lib-dir))
