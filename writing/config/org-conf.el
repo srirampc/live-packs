ab376ec1b12c9aa474156866a7e7985a8217b8e4
Sriram P C <sriram.pc@gmail.com>
2013-08-05 19:48:06 +0530

    update org mode to 8.0 and the config too

--- writing/config/org-conf.el
+++ writing/config/org-conf.el
@@ -1,5 +1,5 @@
-(require 'org-install)
-(require 'org-latex)
+;;(require 'org-install)
+;;(require 'org-latex)
 (setq org-clock-persist 'history)
 
 (defun is-leos? ()
@@ -43,34 +43,35 @@
 
 ;; Originally taken from Bruno Tavernier: http://thread.gmane.org/gmane.emacs.orgmode/31150/focus=31432
 ;; but adapted to use latexmk 4.20 or higher.
-(defun my-auto-tex-cmd ()
+(defun my-auto-tex-cmd (inarg)
   "When exporting from .org with latex, automatically run latex,
      pdflatex, or xelatex as appropriate, using latexmk."
   (let ((texcmd)))
   ;; default command: oldstyle latex via dvi
-  (setq texcmd "latexmk -dvi -pdfps -quiet %f")
+  (setq texcmd "latexmk -dvi -pdfps -quiet -output-directory=%o %f")
   ;; pdflatex -> .pdf
   (if (string-match "LATEX_CMD: pdflatex" (buffer-string))
-      (setq texcmd "latexmk -pdf -quiet %f"))
+      (setq texcmd "latexmk -pdf -quiet -output-directory=%o %f"))
   ;; xelatex -> .pdf
   (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-      (setq texcmd "latexmk -pdflatex=xelatex -pdf -quiet %f"))
+      (setq texcmd "latexmk -pdflatex=xelatex -pdf -quiet -output-directory=%o %f"))
   ;; LaTeX compilation command
-  (setq org-latex-to-pdf-process (list texcmd)))
+  (setq org-latex-pdf-process (list texcmd)))
 
-(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-cmd)
+(add-hook 'org-export-before-processing-hook 'my-auto-tex-cmd)
+;;(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-cmd)
 
 
 ;; Specify default packages to be included in every tex file, whether pdflatex or xelatex
-(setq org-export-latex-packages-alist
+(setq org-latex-packages-alist
       '(("" "graphicx" t)
         ("" "longtable" nil)
         ("" "float" nil)))
 
-(defun my-auto-tex-parameters ()
+(defun my-auto-tex-parameters (inarg)
       "Automatically select the tex packages to include."
       ;; default packages for ordinary latex or pdflatex export
-      (setq org-export-latex-default-packages-alist
+      (setq org-latex-default-packages-alist
             '(("AUTO" "inputenc" t)
               ("T1"   "fontenc"   t)
               (""     "fixltx2e"  nil)
@@ -83,65 +84,69 @@
               (""     "amssymb"   t)
               (""     "hyperref"  nil)))
 
-      (if (string-match "LATEX_PROJECT: kamban" (buffer-string))
-          (setq org-export-latex-default-packages-alist
-                '(
-                  )))
-
-      (if (string-match "LATEX_PROJECT: kamban" (buffer-string))
-          (setq org-export-latex-classes
-                (cons '("book"
-                        "\\documentclass[11pt,draft,twoside,a4paper]{kamban}"
-                        ("\\part{%s}" . "\\part*{%s}")
-                        ("\\chapter{%s}" . "\\chapter*{%s}")
-                        ("\\section{%s}" . "\\section*{%s}")
-                        ("\\subsection{%s}" . "\\subsection*{%s}")
-                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-                      org-export-latex-classes)))
-
       (if (string-match "LATEX_LANG: tamil" (buffer-string))
-          (setq org-export-latex-default-packages-alist
+          (setq org-latex-default-packages-alist
                 '(("" "fontspec" t)
                   ("" "xunicode" t)
                   ("" "xltxtra" t)
                   ("xetex" "hyperref" nil)
                   )))
 
-      (if (string-match "LATEX_LANG: tamil" (buffer-string))
-          (setq org-export-latex-classes
-                (cons '("article"
-                        "\\documentclass[11pt,article,oneside]{memoir}"
-                        ("\\section{%s}" . "\\section*{%s}")
-                        ("\\subsection{%s}" . "\\subsection*{%s}")
-                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-                      org-export-latex-classes)))
-
       ;; Packages to include when xelatex is used
       (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-          (setq org-export-latex-default-packages-alist
-                '(("" "fontspec" t)
-                  ("" "xunicode" t)
-                  ("" "url" t)
+          (setq org-latex-default-packages-alist
+                '(("" "url" t)
                   ("" "rotating" t)
                   ("american" "babel" t)
                   ("babel" "csquotes" t)
                   ("" "soul" t)
                   ("xetex" "hyperref" nil)
-                  )))
+                  ))))
 
-      (if (string-match "LATEX_CMD: xelatex" (buffer-string))
-          (setq org-export-latex-classes
-                (cons '("article"
-                        "\\documentclass[11pt,article,oneside]{memoir}"
-                        ("\\section{%s}" . "\\section*{%s}")
-                        ("\\subsection{%s}" . "\\subsection*{%s}")
-                        ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                        ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                        ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
-                      org-export-latex-classes))))
+(add-hook 'org-export-before-processing-hook 'my-auto-tex-parameters)
 
-(add-hook 'org-export-latex-after-initial-vars-hook 'my-auto-tex-parameters)
+(setq org-latex-classes
+      (cons '("aps-article"
+              "\\documentclass[11pt,article,oneside]{memoir}
+              [DEFAULT-PACKAGES]
+              [PACKAGES]
+              [EXTRA]"
+              ("\\chapter{%s}" . "\\chapter*{%s}")
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+            org-latex-classes))
+
+(setq org-latex-classes
+      (cons '("kamban-book"
+              "\\documentclass[11pt,draft,twoside,a4paper]{kamban}"
+              ("\\part{%s}" . "\\part*{%s}")
+              ("\\chapter{%s}" . "\\chapter*{%s}")
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+            org-latex-classes))
+
+(setq org-latex-classes
+      (cons '("tamil-article"
+              "\\documentclass[11pt,article,oneside]{memoir}"
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+            org-latex-classes))
+
+(setq org-latex-classes
+      (cons '("xelatex-article"
+              "\\documentclass[11pt,article,oneside]{memoir}"
+              ("\\section{%s}" . "\\section*{%s}")
+              ("\\subsection{%s}" . "\\subsection*{%s}")
+              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+              ("\\paragraph{%s}" . "\\paragraph*{%s}")
+              ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+            org-latex-classes))
