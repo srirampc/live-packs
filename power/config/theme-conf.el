5b19b186a56fe71b7578407ed8b2c6261e3b681e
Sriram PC <sriram.pc@gmail.com>
2014-06-10 13:01:36 -0400

    Added theme update for windows

--- power/config/theme-conf.el
+++ power/config/theme-conf.el
@@ -1,7 +1,9 @@
 ;;
  (if (equalp (symbol-name system-type) "gnu/linux")
      (set-default-font "Inconsolata-12")
-   (set-default-font "Monaco-12"))
+   (if (equalp (symbol-name system-type) "windows-nt")
+       (set-default-font "Inconsolata-12")
+     (set-default-font "Monaco-12")))
 
 ;;
 (setq custom-theme-directory (live-pack-lib-dir))
